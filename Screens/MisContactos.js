52c38ba583098360d7ea46c2e0fb819735c469cd
pedro <pfraguas@udesa.edu.ar>
2021-06-24 18:31:22 -0300

    search function

--- Screens/MisContactos.js
+++ Screens/MisContactos.js
@@ -30,6 +30,7 @@ export class MisContactos extends Component {
         this.state = {
             busquedaHandler: "",
             users: [],
+            filteredUsers: [],
             selectItem: null,
             Modal: false,
         }
@@ -67,6 +68,12 @@ export class MisContactos extends Component {
         })
     }
 
+    filterContacts = (query) => {
+        query = query.toLowerCase()
+        let users = this.state.users.filter((user) => {return user.name.first.toLowerCase().includes(query) || user.name.last.toLowerCase().includes(query) || user.location.country.toLowerCase().includes(query) || user.location.city.toLowerCase().includes(query)})
+        this.setState({filteredUsers: users})
+    }
+
     render() {
 
         return(
@@ -82,7 +89,7 @@ export class MisContactos extends Component {
             <View style={[{display:'flex'},{flexDirection:'row'}, {margin:14}, {justifyContent:"center"},{alignContent:'center'}]}>
                 <TextInput style={[styles.input,{flex:3},{marginEnd:15}]} placeholder="Buscar" onChangeText={text => this.setState({busquedaHandler: text})}/>
                 {/* Este boton guarda la cantidad ingresada y luego ejecuta la funcion */}
-                <TouchableOpacity style={[{alignSelf: 'center'},{elevation:2}]}>
+                <TouchableOpacity style={[{alignSelf: 'center'},{elevation:2}]} onPress={() => {this.filterContacts(this.state.busquedaHandler)}}>
                     <View style={styles.boton}>
                         <Text style={styles.botonText}>Buscar</Text>
                     </View>
@@ -91,7 +98,7 @@ export class MisContactos extends Component {
             
 
             {/* Esta lista debe mostrar los contactos guardados en local storage */}
-            <ListadeContactos titulo={"Contactos guardados"} usuarios = {this.state.users} deleteContact={this.deleteContact}  showModal = {this.showModal}/>
+            <ListadeContactos titulo={"Contactos guardados"} usuarios = {this.state.filteredUsers} deleteContact={this.deleteContact}  showModal = {this.showModal}/>
 
             </ScrollView>
             
@@ -99,7 +106,10 @@ export class MisContactos extends Component {
 
             {/* Esta boton debe guardar los contactos que traemos de la API */}
             <View style={[{backgroundColor:"transparent"},{position:"absolute"},{bottom:10},{alignSelf: "center"}]}>
-                <TouchableOpacity  style={[styles.botonGuardarContactos,{justifyContent:"flex-end"}]} onPress={() => {getLocal('localUsers').then((users)=>{this.setState({users: users})})}}>
+                <TouchableOpacity  style={[styles.botonGuardarContactos,{justifyContent:"flex-end"}]} onPress={() => {getLocal('localUsers').then((users)=>{
+                    this.setState({users: users})
+                    this.setState({filteredUsers: users})
+                    })}}>
                         <View style={styles.textIconContainer}>
                             <Text style={[{alignSelf: 'center'}, {justifyContent: 'center'},styles.botonText]}>Cargar contactos</Text>
                             <Image style={styles.icono} source={require('../src/Icons/Synchronize.png')}/>
