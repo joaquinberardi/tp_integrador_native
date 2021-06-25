1305b48295bf24edca17e0065711da9ce5445316
pedro <pfraguas@udesa.edu.ar>
2021-06-25 03:07:51 -0300

    lowercase variables

--- Screens/MisContactos.js
+++ Screens/MisContactos.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import {styles} from '../src/Styles';
 
 import {
     View,
     Text,
-    FlatList,
-    Modal,
-    Button,
     TextInput,
     TouchableOpacity,
     Image,
     ScrollView,
-    SafeAreaView 
 } from 'react-native';
 
 import {ListadeContactos} from '../Components/ListadeContactos';
 import {Header} from '../Components/Header';
 import {ModalContacto} from '../Components/ModalContacto';
 
-
-import {getAPI} from '../api/RandomUser';
 import {storeLocal} from '../api/storeLocal';
 import {getLocal} from '../api/getLocal';
-
 
 export class MisContactos extends Component {
     constructor(){
@@ -33,7 +25,7 @@ export class MisContactos extends Component {
             users: [],
             filteredUsers: [],
             selectItem: null,
-            Modal: false,
+            modal: false,
             Edit: false,
         }
     }
@@ -63,11 +55,11 @@ export class MisContactos extends Component {
     showModal = (key) => {
         let user = this.state.users.filter((user) => { return user.login.uuid === key})
         this.setState({selectItem: user[0]});
-        this.setState({Modal: true});
+        this.setState({modal: true});
     }
 
     closeModal = () => {
-        this.setState({Modal: false})
+        this.setState({modal: false})
     }
 
     addComment = (key,comment) => {
@@ -88,33 +80,28 @@ export class MisContactos extends Component {
     render() {
 
         return(
-            <View style={{flex:1}}>
+        <View style={{flex:1}}>
 
-            {/* <Header titulo = {"Mis Contactos"} /> */}
-            
             <Header titulo={"Mis contactos"} navigation={this.props.navigation}/>
-            
 
             <ScrollView>
-            {/* El cant handler se ocupa de pedirle cierta cantidad de usuarios a la api? */}
-            <View style={[{display:'flex'},{flexDirection:'row'}, {margin:14}, {justifyContent:"center"},{alignContent:'center'}]}>
-                <TextInput style={[styles.input,{flex:3},{marginEnd:15}]} placeholder="Buscar" onChangeText={text => this.setState({busquedaHandler: text})}/>
-                {/* Este boton guarda la cantidad ingresada y luego ejecuta la funcion */}
-                <TouchableOpacity style={[{alignSelf: 'center'},{elevation:2}]} onPress={() => {this.filterContacts(this.state.busquedaHandler)}}>
-                    <View style={styles.boton}>
-                        <Text style={styles.botonText}>Buscar</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-            
-
-            {/* Esta lista debe mostrar los contactos guardados en local storage */}
-            <ListadeContactos titulo={"Contactos guardados"} usuarios={this.state.filteredUsers} bin={false} deleteContact={this.deleteContact} showModal = {this.showModal}/>
+                {/* El cant handler se ocupa de pedirle cierta cantidad de usuarios a la api? */}
+                <View style={[{display:'flex'},{flexDirection:'row'}, {margin:14}, {justifyContent:"center"},{alignContent:'center'}]}>
+                    <TextInput style={[styles.input,{flex:3},{marginEnd:15}]} placeholder="Buscar" onChangeText={text => this.setState({busquedaHandler: text})}/>
+                    {/* Este boton guarda la cantidad ingresada y luego ejecuta la funcion */}
+                    <TouchableOpacity style={[{alignSelf: 'center'},{elevation:2}]} onPress={() => {this.filterContacts(this.state.busquedaHandler)}}>
+                        <View style={styles.boton}>
+                            <Text style={styles.botonText}>Buscar</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                
+                {/* Esta lista debe mostrar los contactos guardados en local storage */}
+                <ListadeContactos titulo={"Contactos guardados"} usuarios={this.state.filteredUsers} bin={false} deleteContact={this.deleteContact} showModal = {this.showModal}/>
 
             </ScrollView>
 
-            
-            <ModalContacto selectItem={this.state.selectItem} Modal={this.state.Modal} closeModal={this.closeModal} addComment={this.addComment} updateContact={this.updateContact} allowEdit={true}/>
+            <ModalContacto selectItem={this.state.selectItem} modal={this.state.modal} closeModal={this.closeModal} addComment={this.addComment} updateContact={this.updateContact} allowEdit={true}/>
             
             {/* Esta boton debe guardar los contactos que traemos de la API */}
             <View style={[{backgroundColor:"transparent"},{position:"absolute"},{bottom:10},{alignSelf: "center"}]}>
@@ -127,11 +114,9 @@ export class MisContactos extends Component {
                             <Image style={styles.icono} source={require('../src/Icons/Synchronize.png')}/>
                         </View>
                 </TouchableOpacity>
-            </View>
-
-
-            
+            </View>            
 
         </View>
-    )}
+        )
+    }
 }
--- Screens/PapeleraDeReciclaje.js
+++ Screens/PapeleraDeReciclaje.js
@@ -4,10 +4,6 @@ import {styles} from '../src/Styles';
 import {
     View,
     Text,
-    FlatList,
-    Modal,
-    Button,
-    TextInput,
     TouchableOpacity,
     Image,
     ScrollView,
@@ -17,7 +13,7 @@ import {ListadeContactos} from '../Components/ListadeContactos';
 import {Header} from '../Components/Header';
 import {getLocal} from '../api/getLocal';
 import {ModalContacto} from '../Components/ModalContacto';
-import { storeLocal } from '../api/storeLocal';
+import {storeLocal} from '../api/storeLocal';
 
 
 export class PapeleraDeReciclaje extends Component {
@@ -26,22 +22,18 @@ export class PapeleraDeReciclaje extends Component {
         this.state = {
             users: [],
             selectItem: null,
-            Modal: false,
+            modal: false,
         }
-    }
-
-    componentDidMount(){
-        //getLocal('recycleBin').then((users) => {this.setState({users: users})})
     }
 
     showModal = (key) => {
         let user = this.state.users.filter((user) => { return user.login.uuid === key})
-        this.setState({selectItem: user[0]});
-        this.setState({Modal: true});
+        this.setState({selectItem: user[0]})
+        this.setState({modal: true})
     }
 
     closeModal = () => {
-        this.setState({Modal: false})
+        this.setState({modal: false})
     }
 
     emptyBin = () => {
@@ -72,25 +64,25 @@ export class PapeleraDeReciclaje extends Component {
                 <ListadeContactos titulo={"Contactos eliminados"} usuarios={this.state.users} bin={true} showModal = {this.showModal} recoverContact={this.recoverContact}/>
             </ScrollView>
             
-            <ModalContacto selectItem={this.state.selectItem} Modal={this.state.Modal} closeModal={this.closeModal} allowEdit={false}/>
+            <ModalContacto selectItem={this.state.selectItem} modal={this.state.modal} closeModal={this.closeModal} allowEdit={false}/>
 
             {/* Esta boton debe guardar los contactos que traemos de la API */}
             <View style={[{backgroundColor:"transparent"},{position:"absolute"},{bottom:10},{alignSelf: "center"}]}>
                 <TouchableOpacity  style={[styles.botonGuardarContactos,{justifyContent:"flex-end"}]} onPress={() => {getLocal('recycleBin').then((users)=>{this.setState({users: users})})}}>
-                        <View style={styles.textIconContainer}>
-                            <Text style={[{alignSelf: 'center'}, {justifyContent: 'center'},styles.botonText]}>Actualizar</Text>
-                            <Image style={styles.icono} source={require('../src/Icons/Synchronize.png')}/>
-                        </View>
+                    <View style={styles.textIconContainer}>
+                        <Text style={[{alignSelf: 'center'}, {justifyContent: 'center'},styles.botonText]}>Actualizar</Text>
+                        <Image style={styles.icono} source={require('../src/Icons/Synchronize.png')}/>
+                    </View>
                 </TouchableOpacity>
                 <TouchableOpacity  style={[styles.botonGuardarContactos,{justifyContent:"flex-end"}]} onPress={() => {this.emptyBin()}}>
-                        <View style={styles.textIconContainer}>
-                            <Text style={[{alignSelf: 'center'}, {justifyContent: 'center'},styles.botonText]}>Vaciar Papelera</Text>
-                            <Image style={styles.icono} source={require('../src/Icons/Empty.png')}/>
-                        </View>
+                    <View style={styles.textIconContainer}>
+                        <Text style={[{alignSelf: 'center'}, {justifyContent: 'center'},styles.botonText]}>Vaciar Papelera</Text>
+                        <Image style={styles.icono} source={require('../src/Icons/Empty.png')}/>
+                    </View>
                 </TouchableOpacity>
             </View>
 
-
         </View>
-    )}
+        )
+    }
 }
